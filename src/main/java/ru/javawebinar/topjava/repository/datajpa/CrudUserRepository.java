04d083061fda17a8b3bffa66383fd49cc4ac43eb
kortov <kortovea@gmail.com>
2018-09-09 22:39:07 +0400

    7 05 fix hint graph

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudUserRepository.java
@@ -30,7 +30,8 @@ public interface CrudUserRepository extends JpaRepository<User, Integer> {
 //    @Query("SELECT u FROM User u LEFT JOIN FETCH u.meals WHERE u.id = ?1")
 //    @EntityGraph(value = User.GRAPH_WITH_MEALS)
 
-    @EntityGraph(attributePaths = {"meals", "roles"})
+    //    https://stackoverflow.com/a/46013654/548473
+    @EntityGraph(attributePaths = {"meals"}, type = EntityGraph.EntityGraphType.LOAD)
     @Query("SELECT u FROM User u WHERE u.id=?1")
     User getWithMeals(int id);
 }
--- src/main/java/ru/javawebinar/topjava/repository/jpa/JpaUserRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jpa/JpaUserRepositoryImpl.java
@@ -1,14 +1,14 @@
 package ru.javawebinar.topjava.repository.jpa;
 
-import org.springframework.dao.support.DataAccessUtils;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-import ru.javawebinar.topjava.model.User;
-import ru.javawebinar.topjava.repository.UserRepository;
+import org.hibernate.jpa.*;
+import org.springframework.dao.support.*;
+import org.springframework.stereotype.*;
+import org.springframework.transaction.annotation.*;
+import ru.javawebinar.topjava.model.*;
+import ru.javawebinar.topjava.repository.*;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import java.util.List;
+import javax.persistence.*;
+import java.util.*;
 
 @Repository
 @Transactional(readOnly = true)
@@ -61,6 +61,7 @@ public class JpaUserRepositoryImpl implements UserRepository {
     public User getByEmail(String email) {
         List<User> users = em.createNamedQuery(User.BY_EMAIL, User.class)
                 .setParameter(1, email)
+                .setHint(QueryHints.HINT_PASS_DISTINCT_THROUGH, false)
                 .getResultList();
         return DataAccessUtils.singleResult(users);
     }
