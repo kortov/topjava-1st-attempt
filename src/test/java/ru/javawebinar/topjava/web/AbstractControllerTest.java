8a2adb12dc8960a661d5ef714db582cf117d2c5c
kortov <kortovea@gmail.com>
2018-09-09 22:40:06 +0400

    7 07 controller test

---
+++ src/test/java/ru/javawebinar/topjava/web/AbstractControllerTest.java
@@ -0,0 +1,68 @@
+package ru.javawebinar.topjava.web;
+
+import org.junit.*;
+import org.junit.runner.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.cache.*;
+import org.springframework.test.context.*;
+import org.springframework.test.context.junit4.*;
+import org.springframework.test.context.web.*;
+import org.springframework.test.web.servlet.*;
+import org.springframework.test.web.servlet.setup.*;
+import org.springframework.transaction.annotation.*;
+import org.springframework.web.context.*;
+import org.springframework.web.filter.*;
+import ru.javawebinar.topjava.*;
+import ru.javawebinar.topjava.repository.*;
+import ru.javawebinar.topjava.service.*;
+
+import javax.annotation.*;
+
+@ContextConfiguration({
+        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-mvc.xml",
+        "classpath:spring/spring-db.xml"
+})
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+@ActiveProfiles(resolver = AllActiveProfileResolver.class)
+abstract public class AbstractControllerTest {
+
+    private static final CharacterEncodingFilter CHARACTER_ENCODING_FILTER = new CharacterEncodingFilter();
+
+    static {
+        CHARACTER_ENCODING_FILTER.setEncoding("UTF-8");
+        CHARACTER_ENCODING_FILTER.setForceEncoding(true);
+    }
+
+    protected MockMvc mockMvc;
+
+    @Autowired
+    private CacheManager cacheManager;
+
+    @Autowired(required = false)
+    private JpaUtil jpaUtil;
+
+    @Autowired
+    protected UserService userService;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @PostConstruct
+    private void postConstruct() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
+                .addFilter(CHARACTER_ENCODING_FILTER)
+                .build();
+    }
+
+    @Before
+    public void setUp() {
+        cacheManager.getCache("users").clear();
+        if (jpaUtil != null) {
+            jpaUtil.clear2ndLevelHibernateCache();
+        }
+    }
+}
